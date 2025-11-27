bac2881996c5a420303b1d76373140887fa8eb04
chandiraleka-design <pparameswaran558@gmail.com>
2025-11-27 11:47:24 +0530

    Add files via upload

---
+++ project new.py
@@ -0,0 +1,36 @@
+Python 3.13.7 (tags/v3.13.7:bcee1c3, Aug 14 2025, 14:15:11) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> 
+= RESTART: C:/Users/ppara/AppData/Local/Programs/Python/Python313/new project.py
+
+=== Bayesian Optimization (EI) ===
+Iteration 1/10  |  Best so far: 0.2188
+Iteration 2/10  |  Best so far: 0.2152
+Iteration 3/10  |  Best so far: 0.2113
+Iteration 4/10  |  Best so far: 0.2113
+Iteration 5/10  |  Best so far: 0.2113
+Iteration 6/10  |  Best so far: 0.2113
+Iteration 7/10  |  Best so far: 0.2113
+Iteration 8/10  |  Best so far: 0.2110
+Iteration 9/10  |  Best so far: 0.2110
+Iteration 10/10  |  Best so far: 0.2110
+Best Params (EI): [3.50071737e+02 2.57778196e-02 9.21178646e+00 5.56955278e-01
+ 6.47183408e-01]
+Best Score (EI): 0.21100595408556946
+
+=== Bayesian Optimization (UCB) ===
+Iteration 1/10  |  Best so far: 0.2276
+Iteration 2/10  |  Best so far: 0.2276
+Iteration 3/10  |  Best so far: 0.2269
+Iteration 4/10  |  Best so far: 0.2164
+Iteration 5/10  |  Best so far: 0.2164
+Iteration 6/10  |  Best so far: 0.2164
+Iteration 7/10  |  Best so far: 0.2159
+Iteration 8/10  |  Best so far: 0.2159
+Iteration 9/10  |  Best so far: 0.2159
+Iteration 10/10  |  Best so far: 0.2159
+Best Params (UCB): [2.67095774e+02 9.76753661e-02 7.44054067e+00 9.96448304e-01
+ 5.04967479e-01]
+Best Score (UCB): 0.21593007548459012
+
+=== Random Search Baseline ===
